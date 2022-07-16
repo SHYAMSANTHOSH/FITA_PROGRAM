13c7b62d93dde77c87c1f091ed7dad5acc91d247
SHYAMSANTHOSH <89693199+SHYAMSANTHOSH@users.noreply.github.com>
2022-07-16 12:27:12 +0530

    Add files via upload

---
+++ FITA_PROGRAMS/src/com/basic/SampleProgram.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class SampleProgram {
+	
+	public static void add(int c,int d) {
+		System.out.println(c+d);
+	}
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b = 5;
+
+		add(a,b);
+
+	}
+
+}
