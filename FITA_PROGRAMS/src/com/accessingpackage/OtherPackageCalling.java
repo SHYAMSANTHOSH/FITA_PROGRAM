9850b2870ead361971b3917717c93fbdb09c67c4
SHYAMSANTHOSH <89693199+SHYAMSANTHOSH@users.noreply.github.com>
2022-07-16 12:26:30 +0530

    Add files via upload

---
+++ FITA_PROGRAMS/src/com/accessingpackage/OtherPackageCalling.java
@@ -0,0 +1,17 @@
+package com.accessingpackage;
+
+import com.basic.*;
+
+public class OtherPackageCalling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Calling Exception Package");
+		
+		
+		SampleProgram s = new SampleProgram();
+		s.add(10,5);
+	}
+
+}
